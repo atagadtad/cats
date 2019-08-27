62d6e7dec3bc9318e4e81eb3802633e4aaeb1900
atagadtad <aarontagadtad@gmail.com>
2019-08-27 23:28:24 +0000

    finished the assignment with @andrew and @karl

---
+++ test/asyncBreedsTest.js
@@ -0,0 +1,17 @@
+// asyncBreedsTest.js
+const assert = require('chai').assert;
+const breedDetailsFromFile = require('../asyncBreeds');
+
+describe('#breedDetailsFromFile', () => {
+  it('provides, via callback, breed details for the Bombay breed', () => {
+    console.log(breedDetailsFromFile);
+  });
+  it('provides, via callback, undefined for a breed that does not exist', (done) => {
+    breedDetailsFromFile('Saphire', (desc) => {
+
+      assert.equal(undefined, desc);
+      done();
+    });
+  });
+
+});
